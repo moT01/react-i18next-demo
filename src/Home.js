38097dd31faffd7b917d0a2f212bed87a8cf2d12
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-18 12:53:31 -0600

    fix: indentation

--- src/Home.js
+++ src/Home.js
@@ -2,46 +2,46 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Home extends Component {
-	constructor() {
+  constructor() {
     super();
 
     this.state = {
       username: '',
-			submitted: false
+      submitted: false
     };
-	}
+  }
 
-	updateUsername(e) {
-		this.setState({
-			username: e.target.value
-		})
-	}
+  updateUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
 
-	setUsername() {
-		this.setState({
-			submitted: true
-		})
-	}
+  setUsername() {
+    this.setState({
+      submitted: true
+    })
+  }
 
-	render() {
-		const { submitted, username } = this.state;
+  render() {
+    const { submitted, username } = this.state;
 
-		return (
-			<div className='body'>
-				{submitted ? (
-					<p>Welcome, {username}</p>
-				) : (
-					<p>
-						Enter your username:
-						<input type='text' onChange={this.updateUsername.bind(this)} />
-						<button onClick={this.setUsername.bind(this)}>Submit</button>
-					</p>
-				)}
+    return (
+      <div className='body'>
+        {submitted ? (
+          <p>Welcome, {username}</p>
+        ) : (
+          <p>
+            <label>Enter your username:</label>
+            <input type='text' onChange={this.updateUsername.bind(this)} />
+            <button onClick={this.setUsername.bind(this)}>Submit</button>
+          </p>
+        )}
 
-				<p>Go to <Link to='/page2'>page 2</Link></p>
-			</div>
-		);
-	}
+        <p>Go to <Link to='/page2'>page 2</Link></p>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/Page2.js
+++ src/Page2.js
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 
 const Page2 = () => (
-	<div className='body'>
-		<p>This is <strong>page 2</strong></p>
-		<p>Go to <Link to='/'>the home page</Link></p>
-	</div>
+  <div className='body'>
+    <p>This is <strong>page 2</strong></p>
+    <p>Go to <Link to='/'>the home page</Link></p>
+  </div>
 );
 
 export default Page2;
