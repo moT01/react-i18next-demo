56037c01a0bb1c1bc862b56fc675d44b432b849f
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-18 12:14:58 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+import Nav from './Nav';
+import Home from './Home';
+import Page2 from './Page2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Nav />
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/page2' component={Page2}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+	constructor() {
+    super();
+
+    this.state = {
+      username: '',
+			submitted: false
+    };
+	}
+
+	updateUsername(e) {
+		this.setState({
+			username: e.target.value
+		})
+	}
+
+	setUsername() {
+		this.setState({
+			submitted: true
+		})
+	}
+
+	render() {
+		const { submitted, username } = this.state;
+
+		return (
+			<div className='body'>
+				{submitted ? (
+					<p>Welcome, {username}</p>
+				) : (
+					<p>
+						Enter your username:
+						<input type='text' onChange={this.updateUsername.bind(this)} />
+						<button onClick={this.setUsername.bind(this)}>Submit</button>
+					</p>
+				)}
+
+				<p>Go to <Link to='/page2'>page 2</Link></p>
+			</div>
+		);
+	}
+}
+
+export default Home;
