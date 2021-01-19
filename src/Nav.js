2f05a6982879d744918bd7dedaf77881a6858b94
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-18 18:00:59 -0600

    fix: spacing

--- src/Nav.js
+++ src/Nav.js
@@ -4,8 +4,8 @@ const Nav = () => (
   <header className='nav'>
     <div>i18n React Demo</div>
     <div>
-        <Link to="/">Home</Link>
-        <Link to="/page2">Page 2</Link>
+      <Link to="/">Home</Link>
+      <Link to="/page2">Page 2</Link>
     </div>
   </header>
 );
